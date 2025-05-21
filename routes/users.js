e25b8e0558445f80a796382bf760a0359a49a27e
Rashad <rashad.j.2014@gmail.com>
2025-05-21 13:33:28 -0400

    routes fixed

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const { auth } = require("../middlewares/auth");
+const auth = require("../middlewares/auth");
 
-const { getCurrentUser, updateProfile } = require("../controllers/users.js");
+const { getCurrentUser, updateProfile } = require("../controllers/users");
 
 router.get("/me", auth, getCurrentUser);
 router.patch("/me", auth, updateProfile);
